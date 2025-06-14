0df67bd298b71bc3e9394fc84966ee5356766d3d
AlexMan123456 <alex.man03888@gmail.com>
2025-06-14 18:09:51 +0100

    Add dark mode toggle

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -2,6 +2,7 @@ import IconButton from "@mui/material/IconButton";
 import { useColorScheme } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import { DarkMode, LightMode } from "@mui/icons-material";
+import Tooltip from "@mui/material/Tooltip";
 
 function DarkModeToggle() {
   const { mode, setMode } = useColorScheme();
@@ -13,17 +14,19 @@ function DarkModeToggle() {
   }, [isDarkMode]);
 
   return (
-    <IconButton
-      sx={{ marginLeft: "auto" }}
-      onClick={() => {
-        setIsDarkMode((darkMode) => {
-          return !darkMode;
-        });
-      }}
-      aria-label={`Enable ${isDarkMode ? "light" : "dark"} mode`}
-    >
-      {mode === "dark" ? <LightMode /> : <DarkMode />}
-    </IconButton>
+    <Tooltip title={`Enable ${isDarkMode ? "light" : "dark"} mode`}>
+      <IconButton
+        sx={{ marginLeft: "auto" }}
+        onClick={() => {
+          setIsDarkMode((darkMode) => {
+            return !darkMode;
+          });
+        }}
+        aria-label={`Enable ${isDarkMode ? "light" : "dark"} mode`}
+      >
+        {mode === "dark" ? <LightMode /> : <DarkMode />}
+      </IconButton>
+    </Tooltip>
   );
 }
 
