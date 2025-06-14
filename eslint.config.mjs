96da71d6bbb9d91b60fbf952bd876170291e9914
AlexMan123456 <alex.man03888@gmail.com>
2025-06-15 00:29:52 +0100

    Remove comment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,8 +21,6 @@ export default tseslint.config(
     },
     settings: {
       "import/resolver": {
-        // You will also need to install and configure the TypeScript resolver
-        // See also https://github.com/import-js/eslint-import-resolver-typescript#configuration
         typescript: true,
         node: true,
       },
