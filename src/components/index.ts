f18d2d8f4c33729e9c945bd4ab04f5096b4ae719
AlexMan123456 <alex.man03888@gmail.com>
2025-06-14 22:15:42 +0100

    Remove unused import (how did the linter not pick this up?)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export { default as DropdownMenu } from "./DropdownMenu";
 export { default as NavigationDrawer } from "./NavigationDrawer";
-export { default as DarkModeToggle } from "./DarkModeToggle";
