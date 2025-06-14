007e4925f9630ee8affa589fe4b35d3847fefabb
AlexMan123456 <alex.man03888@gmail.com>
2025-06-15 00:40:38 +0100

    You forgot to export the damn component you idiot Alex

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as DropdownMenu } from "src/components/DropdownMenu";
 export { default as NavigationDrawer } from "src/components/NavigationDrawer";
+export { default as DarkModeToggle } from "src/components/DarkModeToggle";
