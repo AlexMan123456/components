7da576423c0c653b87892db92e9a4f1468de6557
AlexMan123456 <alex.man03888@gmail.com>
2025-06-21 15:27:24 +0100

    Add borders around playground component

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,3 +1,5 @@
+import Box from "@mui/material/Box";
+import { SxProps, Theme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import { stripIndent } from "common-tags";
 import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
@@ -5,18 +7,49 @@ import { LiveEditor, LiveError, LivePreview, LiveProvider } from "react-live";
 export interface PlaygroundProps {
   code: string;
   scope: Record<string, unknown>;
+  previewStyles?: SxProps<Theme>;
+  editorStyles?: SxProps<Theme>;
 }
 
-function Playground({ code, scope }: PlaygroundProps) {
+function Playground({
+  code,
+  scope,
+  previewStyles,
+  editorStyles,
+}: PlaygroundProps) {
+  const defaultPreviewStyles: SxProps<Theme> = {
+    backgroundColor: "black",
+    border: 0.3,
+    borderRadius: 1,
+    paddingLeft: 2,
+    borderColor: "darkgray",
+  };
+  const defaultEditorStyles: SxProps<Theme> = {
+    border: 0.3,
+    borderRadius: 0.3,
+    borderColor: "darkgray",
+  };
+  const allPreviewStyles = previewStyles
+    ? { ...defaultPreviewStyles, ...previewStyles }
+    : { ...defaultPreviewStyles };
+  const allEditorStyles = editorStyles
+    ? { ...defaultEditorStyles, ...editorStyles }
+    : { ...defaultEditorStyles };
   return (
-    <LiveProvider code={stripIndent(code)} scope={scope}>
-      <Typography variant="h5">Result</Typography>
-      <LivePreview />
-      <LiveError />
-      <br />
-      <Typography variant="h5">Code</Typography>
-      <LiveEditor />
-    </LiveProvider>
+    <Box sx={{ borderRadius: 1, border: 0.5, padding: 2 }}>
+      <LiveProvider code={stripIndent(code)} scope={scope}>
+        <Typography variant="h5">Result</Typography>
+        <Box sx={allPreviewStyles}>
+          <LivePreview />
+        </Box>
+        <LiveError />
+        <br />
+        <Typography variant="h5">Code</Typography>
+        <Box sx={allEditorStyles}>
+          <LiveEditor />
+        </Box>
+      </LiveProvider>
+    </Box>
   );
 }
 
