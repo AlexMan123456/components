da76cdf3113952b9fb82a661482c72add7284dde
AlexMan123456 <alex.man03888@gmail.com>
2025-06-22 01:22:04 +0100

    Add noInline to playground, allowing for more than just the return of JSX/TSX to be shown

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as DropdownMenu } from "src/components/DropdownMenu";
 export { default as NavigationDrawer } from "src/components/NavigationDrawer";
 export { default as DarkModeToggle } from "src/components/DarkModeToggle";
-export { default as Playground } from "src/components/Playground";
+export { default as ReactPlayground } from "src/components/ReactPlayground";
 export { default as InternalLink } from "src/components/InternalLink";
 export { default as ExternalLink } from "src/components/ExternalLink";
