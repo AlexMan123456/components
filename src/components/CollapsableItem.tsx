718f1b717920bc6cf06ffbe80e8d44f554962c08
AlexMan123456 <alex.man03888@gmail.com>
2025-07-10 23:46:45 +0100

    Add CollapsableItem component

---
+++ src/components/CollapsableItem.tsx
@@ -0,0 +1,78 @@
+import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
+import ArrowDropUp from "@mui/icons-material/ArrowDropUp";
+import Box from "@mui/material/Box";
+import ButtonBase from "@mui/material/ButtonBase";
+import Collapse, { CollapseProps } from "@mui/material/Collapse";
+import { type SxProps } from "@mui/material/styles";
+import { type ElementType, useEffect, useState, type ReactNode } from "react";
+
+export interface CollapsableItemProps {
+  isInitiallyOpen?: boolean;
+  onOpen?: () => void;
+  onClose?: () => void;
+  children: ReactNode;
+  buttonStyles?: SxProps;
+  buttonContents: ReactNode;
+  buttonComponent?: ElementType;
+  openIcon?: ReactNode;
+  closedIcon?: ReactNode;
+  collapseProps?: Omit<CollapseProps, "in">;
+}
+
+function CollapsableItem({
+  isInitiallyOpen,
+  onOpen,
+  onClose,
+  children,
+  buttonStyles,
+  buttonContents,
+  buttonComponent: ButtonComponent = ButtonBase,
+  collapseProps,
+  openIcon = <ArrowDropUp />,
+  closedIcon = <ArrowDropDown />,
+}: CollapsableItemProps) {
+  const [isItemOpen, setIsItemOpen] = useState<boolean>(!!isInitiallyOpen);
+
+  useEffect(() => {
+    if (isItemOpen && onOpen) {
+      onOpen();
+    } else if (!isItemOpen && onClose) {
+      onClose();
+    }
+  }, [isItemOpen]);
+
+  return (
+    <Box>
+      <ButtonComponent
+        onClick={() => {
+          setIsItemOpen((previouslyOpen) => {
+            return !previouslyOpen;
+          });
+        }}
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          paddingY: 1.5,
+          paddingX: 2,
+          textAlign: "center",
+          "&:hover":
+            ButtonComponent === ButtonBase
+              ? { backgroundColor: "action.hover" }
+              : null,
+          ...buttonStyles,
+        }}
+        aria-expanded={isItemOpen}
+      >
+        {buttonContents}
+        {isItemOpen ? openIcon : closedIcon}
+      </ButtonComponent>
+      <Collapse in={isItemOpen} {...collapseProps}>
+        {children}
+      </Collapse>
+    </Box>
+  );
+}
+
+export default CollapsableItem;
