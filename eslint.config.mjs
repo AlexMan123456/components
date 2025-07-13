a6e9c335d7819a6ee93a92001be4448b59373612
AlexMan123456 <alex.man03888@gmail.com>
2025-07-13 04:01:28 +0100

    Use custom React base config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,30 +4,11 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import importPlugin from "eslint-plugin-import";
 import reactPlugin from "eslint-plugin-react";
-import alexBaseConfig from "@alextheman/eslint-config-typescript-base"
+import alexBaseConfig from "@alextheman/eslint-config-typescript-react-base"
 
 export default [
-  js.configs.recommended,
   ...alexBaseConfig,
   {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      import: importPlugin,
-      react: reactPlugin,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "react-hooks/exhaustive-deps": "off"
-    },
+    files: ["**/*.{ts,tsx}"]
   },
 ];
