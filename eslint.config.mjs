f0d9c1155625cbe9c518aded14519c4d20e0adec
AlexMan123456 <alex.man03888@gmail.com>
2025-07-13 04:03:49 +0100

    Get rid of unneeded imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,3 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import importPlugin from "eslint-plugin-import";
-import reactPlugin from "eslint-plugin-react";
 import alexBaseConfig from "@alextheman/eslint-config-typescript-react-base"
 
-export default [
-  ...alexBaseConfig,
-  {
-    files: ["**/*.{ts,tsx}"]
-  },
-];
+export default alexBaseConfig;
