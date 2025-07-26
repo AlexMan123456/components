f80a1e52ab7ede12d14a49bc4c66a830410dfe53
AlexMan123456 <alex.man03888@gmail.com>
2025-07-27 00:47:28 +0100

    Make Prettier not Uglier

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,38 +1,31 @@
+export { default as DropdownMenu } from "src/components/DropdownMenu";
+export { type DropdownMenuProps } from "src/components/DropdownMenu";
+
+export { default as NavigationDrawer } from "src/components/NavigationDrawer";
 export {
-  default as DropdownMenu,
-  type DropdownMenuProps,
-} from "src/components/DropdownMenu";
-export {
-  default as NavigationDrawer,
   type NavMenuItem,
   type NavigationDrawerProps,
 } from "src/components/NavigationDrawer";
+
 export { default as DarkModeToggle } from "src/components/DarkModeToggle";
-export {
-  default as ReactPlayground,
-  type ReactPlaygroundProps,
-} from "src/components/ReactPlayground";
-export {
-  default as InternalLink,
-  type InternalLinkProps,
-} from "src/components/InternalLink";
-export {
-  default as ExternalLink,
-  type ExternalLinkProps,
-} from "src/components/ExternalLink";
-export {
-  default as IconWithPopover,
-  type IconWithPopoverProps,
-} from "src/components/IconWithPopover";
-export {
-  default as PopoverText,
-  type PopoverTextProps,
-} from "src/components/PopoverText";
-export {
-  default as CollapsableItem,
-  type CollapsableItemProps,
-} from "src/components/CollapsableItem";
-export {
-  default as ListItemInternalLink,
-  type ListItemInternalLinkProps,
-} from "src/components/ListItemInternalLink";
+
+export { default as ReactPlayground } from "src/components/ReactPlayground";
+export { type ReactPlaygroundProps } from "src/components/ReactPlayground";
+
+export { default as InternalLink } from "src/components/InternalLink";
+export { type InternalLinkProps } from "src/components/InternalLink";
+
+export { default as ExternalLink } from "src/components/ExternalLink";
+export { type ExternalLinkProps } from "src/components/ExternalLink";
+
+export { default as IconWithPopover } from "src/components/IconWithPopover";
+export { type IconWithPopoverProps } from "src/components/IconWithPopover";
+
+export { default as PopoverText } from "src/components/PopoverText";
+export { type PopoverTextProps } from "src/components/PopoverText";
+
+export { default as CollapsableItem } from "src/components/CollapsableItem";
+export { type CollapsableItemProps } from "src/components/CollapsableItem";
+
+export { default as ListItemInternalLink } from "src/components/ListItemInternalLink";
+export { type ListItemInternalLinkProps } from "src/components/ListItemInternalLink";
