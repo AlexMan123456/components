d23816e6cc887470dc3f5ec8147960d1642b3832
AlexMan123456 <alex.man03888@gmail.com>
2025-09-07 15:51:13 +0100

    Add FileInput components from Dropcore
    There's not really any need for a separate package for those files - it just adds one extra package to maintain when there's already so many.
    
    I also do want to change the implementation slightly. I'm not sure how necessary the mapping of file types to file mimes are - we could potentially just use the extensions directly.

---
+++ src/components/FileInputList.tsx
@@ -0,0 +1,63 @@
+import type { Dispatch, SetStateAction } from "react";
+import type { FileInputProps } from "src/components/FileInput";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+
+import FileInput from "src/components/FileInput";
+
+export interface FileInputListProps extends Omit<FileInputProps, "onChange"> {
+  files: File[];
+  setFiles: Dispatch<SetStateAction<File[]>>;
+}
+
+function FileInputList({ files, setFiles, ...fileInputProps }: FileInputListProps) {
+  function onChange(newFiles: File[]) {
+    setFiles((oldFiles) => {
+      return [...oldFiles, ...newFiles];
+    });
+  }
+
+  const newFileInputProps = { ...fileInputProps, onChange };
+  if (newFileInputProps?.multiple === undefined) {
+    newFileInputProps.multiple = true;
+  }
+
+  return (
+    <Box>
+      <FileInput {...newFileInputProps} />
+      <List>
+        {files.map((file) => {
+          return (
+            <ListItem
+              key={file.name}
+              secondaryAction={
+                <IconButton
+                  aria-label="Delete"
+                  edge="end"
+                  onClick={() => {
+                    setFiles((oldFiles) => {
+                      return oldFiles.filter((fileToDelete) => {
+                        return fileToDelete !== file;
+                      });
+                    });
+                  }}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              }
+            >
+              <ListItemText primary={file.name} />
+            </ListItem>
+          );
+        })}
+      </List>
+    </Box>
+  );
+}
+
+export default FileInputList;
