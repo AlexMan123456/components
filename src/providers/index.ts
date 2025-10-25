dc3b06da9a6ec966dc9a066dee64a2fa87955f95
AlexMan123456 <alex.man03888@gmail.com>
2025-10-25 02:42:48 +0100

    Export all prop types for LoaderProvider
    This makes it easier for the Neurosongs wrapper to wrap around them and extend individual interfaces (so the error one can provide apiErrorMap and errorFunction, for example.)

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -5,7 +5,13 @@ export { default as ModeProvider, useMode } from "src/providers/ModeProvider";
 export { default as ScreenSizeProvider, useScreenSize } from "src/providers/ScreenSizeProvider";
 export { default as SnackbarProvider, useSnackbar } from "src/providers/SnackbarProvider";
 
-export type { LoaderProviderProps, LoaderContextValue } from "src/providers/LoaderProvider";
+export type {
+  LoaderProviderProps,
+  LoaderContextValue,
+  LoaderProviderBaseProps,
+  LoaderProviderPropsWithError,
+  LoaderProviderPropsWithNoError,
+} from "src/providers/LoaderProvider";
 export type { LoaderDataProps } from "src/providers/LoaderProvider/LoaderData";
 export type { Mode, ModeProviderProps } from "src/providers/ModeProvider";
 export type { ScreenSizeProps, ScreenSizeContextValue } from "src/providers/ScreenSizeProvider";
