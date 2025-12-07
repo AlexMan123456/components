bf688056804466ea8dcd3495602833f3e17e47b6
AlexMan123456 <alex.man03888@gmail.com>
2025-12-07 18:58:43 +0000

    Add DropdownMenuInternalLink and DropdownMenuExternalLink

---
+++ src/providers/DropdownMenu2/DropdownMenuExternalLink.tsx
@@ -0,0 +1,45 @@
+import type { MenuItemOwnProps } from "@mui/material/MenuItem";
+import type { ComponentProps, MouseEventHandler, ReactNode, Ref } from "react";
+
+import MenuItem from "@mui/material/MenuItem";
+
+import { ExternalLink } from "src/components";
+import { useDropdownMenu } from "src/providers/DropdownMenu2/DropdownMenu2";
+
+export interface DropdownMenuExternalLinkProps extends MenuItemOwnProps {
+  ref?: Ref<HTMLAnchorElement>;
+  href: ComponentProps<typeof ExternalLink>["href"];
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
+  children: ReactNode;
+}
+
+function DropdownMenuExternalLink({
+  ref,
+  href,
+  children,
+  onClick,
+  ...menuItemProps
+}: DropdownMenuExternalLinkProps) {
+  const { closeMenu } = useDropdownMenu();
+
+  return (
+    <MenuItem
+      component={ExternalLink}
+      href={href}
+      ref={ref}
+      {...menuItemProps}
+      onClick={(event) => {
+        if (!event.defaultPrevented) {
+          closeMenu();
+        }
+        if (onClick) {
+          onClick(event);
+        }
+      }}
+    >
+      {children}
+    </MenuItem>
+  );
+}
+
+export default DropdownMenuExternalLink;
---
+++ src/providers/DropdownMenu2/DropdownMenuInternalLink.tsx
@@ -0,0 +1,45 @@
+import type { MenuItemOwnProps } from "@mui/material/MenuItem";
+import type { ComponentProps, MouseEventHandler, ReactNode, Ref } from "react";
+
+import MenuItem from "@mui/material/MenuItem";
+
+import { InternalLink } from "src/components";
+import { useDropdownMenu } from "src/providers/DropdownMenu2/DropdownMenu2";
+
+export interface DropdownMenuInternalLinkProps extends MenuItemOwnProps {
+  ref?: Ref<HTMLAnchorElement>;
+  to: ComponentProps<typeof InternalLink>["to"];
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
+  children: ReactNode;
+}
+
+function DropdownMenuInternalLink({
+  to,
+  ref,
+  children,
+  onClick,
+  ...menuItemProps
+}: DropdownMenuInternalLinkProps) {
+  const { closeMenu } = useDropdownMenu();
+
+  return (
+    <MenuItem
+      component={InternalLink}
+      to={to}
+      ref={ref}
+      {...menuItemProps}
+      onClick={(event) => {
+        if (!event.defaultPrevented) {
+          closeMenu();
+        }
+        if (onClick) {
+          onClick(event);
+        }
+      }}
+    >
+      {children}
+    </MenuItem>
+  );
+}
+
+export default DropdownMenuInternalLink;
